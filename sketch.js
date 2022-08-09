ea41f4ab25c00b2a613df39edb3e373a485a830c
ruchikawhj <74497291+ruchikawhj@users.noreply.github.com>
2022-08-09 18:00:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let engine;
 let world;
 var w1,w2,w3,w4;
 var ball;
-
+var b1,b2;
 function setup() {
   createCanvas(400,400);
   engine = Engine.create();
@@ -20,10 +20,20 @@ function setup() {
   w3=new Ground(200,10,400,20);
   w4=new Ground(390,200,20,400);
 
-  var options={restitution:0.8}
+  var options={restitution:0.2}
 
   ball=Bodies.circle(200,200,20,options);
   World.add(world,ball)
+
+  b1=createImg("up.png");
+  b1.position(50,50);
+  b1.size(50,50);
+  b1.mouseClicked(vForce);
+
+  b2=createImg("right.png");
+  b2.position(250,50);
+  b2.size(50,50);
+  b2.mouseClicked(hForce);
   
 }
 
@@ -38,3 +48,10 @@ function draw()
   ellipse(ball.position.x,ball.position.y,20)
 }
 
+function vForce(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.009})
+}
+
+function hForce(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.009,y:0})
+}
